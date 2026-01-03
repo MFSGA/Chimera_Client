594de250e7eb62666d69782858b7f242eb371eab
MFSGA <chimera_rust@proton.me>
2026-01-03 22:06:33 +0800

    feat(log): complete fn setup_logging 2

--- clash-lib/src/app/logging.rs
+++ clash-lib/src/app/logging.rs
@@ -1,5 +1,8 @@
+use std::sync::Once;
+
 use serde::Serialize;
 use tokio::sync::broadcast::Sender;
+use tracing_log::LogTracer;
 
 use crate::config::def::LogLevel;
 
@@ -19,11 +22,42 @@ impl EventCollector {
     }
 }
 
+// todo: feature(tracing)
+struct LoggingGuard {
+    _file_appender: Option<tracing_appender::non_blocking::WorkerGuard>,
+}
+
+static SETUP_LOGGING: Once = Once::new();
+static mut LOGGING_GUARD: Option<LoggingGuard> = None;
+
 pub fn setup_logging(
     level: LogLevel,
     collector: EventCollector,
     cwd: &str,
     log_file: Option<String>,
 ) {
+    unsafe {
+        SETUP_LOGGING.call_once(|| {
+            LogTracer::init().unwrap_or_else(|e| {
+                eprintln!(
+                    "Failed to init tracing-log: {e}, another env_logger might \
+                     have been initialized"
+                );
+            });
+            LOGGING_GUARD =
+                setup_logging_inner(level, collector, cwd, log_file).unwrap_or_else(|e| {
+                    eprintln!("Failed to setup logging: {e}");
+                    None
+                });
+        });
+    }
+}
+
+fn setup_logging_inner(
+    level: LogLevel,
+    collector: EventCollector,
+    cwd: &str,
+    log_file: Option<String>,
+) -> anyhow::Result<Option<LoggingGuard>> {
     todo!()
 }
