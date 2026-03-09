387b11f1af8f96175a56e4861b3e35c486991079
MFSGA <chimera_rust@proton.me>
2026-03-10 02:36:22 +0800

    feat(dns): add handle_request support

--- clash-dns/src/handler.rs
+++ clash-dns/src/handler.rs
@@ -3,9 +3,11 @@ use std::time::Duration;
 use crate::utils::new_io_error;
 use crate::{DNSListenAddr, DnsMessageExchanger};
 use async_trait::async_trait;
+use hickory_proto::op::{Header, Message, ResponseCode};
 use hickory_server::server::Request;
 use hickory_server::{
     ServerFuture,
+    authority::MessageResponseBuilder,
     server::{RequestHandler, ResponseHandler, ResponseInfo},
 };
 use thiserror::Error;
@@ -24,10 +26,10 @@ struct DnsHandler<X> {
 pub enum DNSError {
     #[error(transparent)]
     Io(#[from] std::io::Error),
-    /* #[error("invalid OP code: {0}")]
+    #[error("invalid OP query: {0}")]
     InvalidOpQuery(String),
     #[error("query failed: {0}")]
-    QueryFailed(String), */
+    QueryFailed(String),
 }
 
 #[async_trait]
@@ -38,9 +40,44 @@ where
     async fn handle_request<H: ResponseHandler>(
         &self,
         request: &Request,
-        response_handle: H,
+        mut response_handle: H,
     ) -> ResponseInfo {
-        todo!()
+        let req = match to_dns_message(request) {
+            Ok(req) => req,
+            Err(err) => {
+                error!("failed to parse dns request: {}", err);
+                return servfail_info();
+            }
+        };
+
+        let resp = match self.exchanger.exchange(&req).await {
+            Ok(resp) => resp,
+            Err(err) => {
+                warn!("dns exchange failed: {}", err);
+                build_servfail_message(&req)
+            }
+        };
+
+        let mut builder = MessageResponseBuilder::from_message_request(request);
+        if let Some(edns) = resp.extensions().clone() {
+            builder.edns(edns);
+        }
+
+        let response = builder.build(
+            resp.header().clone(),
+            resp.answers(),
+            resp.name_servers(),
+            std::iter::empty::<&hickory_proto::rr::Record>(),
+            resp.additionals(),
+        );
+
+        match response_handle.send_response(response).await {
+            Ok(info) => info,
+            Err(err) => {
+                error!("failed to send dns response: {}", err);
+                servfail_info()
+            }
+        }
     }
 }
 
@@ -84,15 +121,18 @@ where
             .is_ok();
     }
     if let Some(c) = listen.doh {
-        todo!()
+        let _ = c;
+        warn!("DoH listener is not implemented yet");
     }
 
     if let Some(c) = listen.dot {
-        todo!()
+        let _ = c;
+        warn!("DoT listener is not implemented yet");
     }
 
     if let Some(c) = listen.doh3 {
-        todo!()
+        let _ = c;
+        warn!("DoH3 listener is not implemented yet");
     }
 
     if !has_server {
@@ -108,4 +148,54 @@ where
             DNSError::Io(new_io_error(format!("dns server error: {x}")))
         })
     }))
+}
+
+fn to_dns_message(request: &Request) -> Result<Message, DNSError> {
+    let mut message = Message::new();
+    message.set_id(request.id());
+    message.set_op_code(request.op_code());
+    message.set_message_type(request.message_type());
+    message.set_authoritative(request.authoritative());
+    message.set_truncated(request.truncated());
+    message.set_recursion_desired(request.recursion_desired());
+    message.set_recursion_available(request.recursion_available());
+    message.set_authentic_data(request.authentic_data());
+    message.set_checking_disabled(request.checking_disabled());
+    message.set_response_code(request.response_code());
+    message.add_queries(request.queries().iter().map(|q| q.original().clone()));
+    message.add_answers(request.answers().iter().cloned());
+    message.add_name_servers(request.name_servers().iter().cloned());
+    message.add_additionals(request.additionals().iter().cloned());
+    if let Some(edns) = request.edns().cloned() {
+        message.set_edns(edns);
+    }
+    Ok(message)
+}
+
+fn build_servfail_message(req: &Message) -> Message {
+    let mut header = Header::response_from_request(req.header());
+    header.set_response_code(ResponseCode::ServFail);
+
+    let mut message = Message::new();
+    message.set_id(header.id());
+    message.set_message_type(header.message_type());
+    message.set_op_code(header.op_code());
+    message.set_authoritative(header.authoritative());
+    message.set_truncated(header.truncated());
+    message.set_recursion_desired(header.recursion_desired());
+    message.set_recursion_available(header.recursion_available());
+    message.set_authentic_data(header.authentic_data());
+    message.set_checking_disabled(header.checking_disabled());
+    message.set_response_code(header.response_code());
+    message.add_queries(req.queries().iter().cloned());
+    if let Some(edns) = req.extensions().clone() {
+        message.set_edns(edns);
+    }
+    message
+}
+
+fn servfail_info() -> ResponseInfo {
+    let mut header = Header::new();
+    header.set_response_code(ResponseCode::ServFail);
+    header.into()
 }
--- clash-lib/src/app/dns/server/mod.rs
+++ clash-lib/src/app/dns/server/mod.rs
@@ -20,9 +20,9 @@ impl chimera_dns::DnsMessageExchanger for DnsMessageExchanger {
         self.resolver.ipv6()
     }
 
-    /* async fn exchange(&self, message: &Message) -> Result<Message, chimera_dns::DNSError> {
+    async fn exchange(&self, message: &Message) -> Result<Message, chimera_dns::DNSError> {
         exchange_with_resolver(&self.resolver, message, true).await
-    } */
+    }
 }
 
 pub async fn get_dns_listener(
@@ -38,8 +38,7 @@ pub async fn get_dns_listener(
                 Ok(()) => Ok(()),
                 Err(err) => {
                     error!("dns listener error: {}", err);
-                    todo!()
-                    // Err(err.into())
+                    Err(crate::Error::Io(std::io::Error::other(err.to_string())))
                 }
             }
         })),
