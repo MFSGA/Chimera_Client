1c28c375c6192854ab49827daf7e06bf2240f61f
MFSGA <chimera_rust@proton.me>
2026-03-19 03:03:24 +0800

    feat(dns): optimize clash-dns

--- clash-dns/src/lib.rs
+++ clash-dns/src/lib.rs
@@ -2,6 +2,7 @@ use std::{net::SocketAddr, path::Path};
 
 use async_trait::async_trait;
 use hickory_proto::op::Message;
+use serde::Deserialize;
 
 mod handler;
 
@@ -9,16 +10,34 @@ mod utils;
 
 pub use handler::{DNSError, get_dns_listener};
 
-// #[derive(Debug, Deserialize, Clone)]
-// #[serde(rename_all = "kebab-case")]
-#[derive(Debug, Clone)]
-pub struct DoHConfig {}
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "kebab-case")]
+pub struct DoHConfig {
+    pub addr: SocketAddr,
+    pub ca_cert: DnsServerCert,
+    pub ca_key: DnsServerKey,
+    pub hostname: Option<String>,
+}
 
-#[derive(Debug, Clone)]
-pub struct DoH3Config {}
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "kebab-case")]
+pub struct DoH3Config {
+    pub addr: SocketAddr,
+    pub ca_cert: DnsServerCert,
+    pub ca_key: DnsServerKey,
+    pub hostname: Option<String>,
+}
 
-#[derive(Debug, Clone)]
-pub struct DoTConfig {}
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "kebab-case")]
+pub struct DoTConfig {
+    pub addr: SocketAddr,
+    pub ca_cert: DnsServerCert,
+    pub ca_key: DnsServerKey,
+}
+
+pub type DnsServerKey = Option<String>;
+pub type DnsServerCert = Option<String>;
 
 #[derive(Debug, Default, Clone)]
 pub struct DNSListenAddr {
