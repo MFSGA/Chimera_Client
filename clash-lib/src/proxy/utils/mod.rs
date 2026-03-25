95ec6104cf5ec2735131a50b786049f62ee03b76
MFSGA <chimera_rust@proton.me>
2026-03-25 09:22:39 +0800

    fix(interface): to solve the interface issue for tun

--- clash-lib/src/proxy/utils/mod.rs
+++ clash-lib/src/proxy/utils/mod.rs
@@ -2,9 +2,7 @@ pub mod socket_helpers;
 
 #[allow(unused_imports)]
 pub use platform::{SocketProtector, clear_socket_protector, set_socket_protector};
-pub(crate) use platform::{
-    maybe_protect_socket, must_bind_socket_on_interface,
-};
+pub(crate) use platform::{maybe_protect_socket, must_bind_socket_on_interface};
 pub use socket_helpers::*;
 
 /// 2
