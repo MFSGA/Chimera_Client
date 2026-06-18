8d9b0f0ba3292330a1f93fa5119a90f988237db8
MFSGA <chimera_rust@proton.me>
2026-06-18 11:33:32 +0800

    feat: add nix development shell

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Chimera Client development environment";
+
+  # Match the nixpkgs revision used by this NixOS installation without
+  # requiring GitHub access when entering the development shell.
+  inputs.nixpkgs.url =
+    "path:/nix/store/pzxxxg9vvzk63122vj38lcmqg9dl6qxk-nixos-26.05.1947.a0374025a863/nixos";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cargo
+          cargo-watch
+          clang
+          cmake
+          git
+          gnumake
+          llvmPackages.libclang
+          nasm
+          ninja
+          pkg-config
+          protobuf
+          rustc
+          rustfmt
+          clippy
+        ];
+
+        buildInputs = with pkgs; [
+          openssl
+        ];
+
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        RUST_BACKTRACE = "1";
+
+        shellHook = ''
+          echo "Chimera Client development environment"
+          echo "Rust: $(rustc --version)"
+          echo "Run: cargo check --workspace"
+        '';
+      };
+    };
+}
